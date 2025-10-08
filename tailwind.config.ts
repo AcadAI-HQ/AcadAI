aeeb4dd7bed613dcf52a83faec9f998797c77d59
bhaskar-j-pathak <bhaskarjy49@gmail.com>
2025-10-08 13:14:36 +0530

    fixing minor shit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,10 @@ export default {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  safelist: [
+    'animate-marquee',
+    'animate-marquee-vertical',
+  ],
   theme: {
     container: {
       center: true,
